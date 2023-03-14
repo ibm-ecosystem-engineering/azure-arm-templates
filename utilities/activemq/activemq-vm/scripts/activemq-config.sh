4807839d96621341bce2fc2d1b640f9af60bddbc
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2023-03-14 12:02:10 +1100

    Updates script and template
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- utilities/activemq/activemq-vm/scripts/activemq-config.sh
+++ utilities/activemq/activemq-vm/scripts/activemq-config.sh
@@ -29,14 +29,14 @@ sudo apt-get update
 sudo apt-get install -y openjdk-11-jre wget
 
 # Install Apache ActiveMQ
-sudo mkdir $ACTIVEMQ_HOME
+sudo mkdir -p $ACTIVEMQ_HOME
 sudo addgroup --quiet --system activemq
 sudo adduser --quiet --system --ingroup activemq --no-create-home --disabled-password activemq
 wget -P $TMP_DIR http://archive.apache.org/dist/activemq/$VERSION/apache-activemq-$VERSION-bin.tar.gz
-sudo tar -xvzf $TMP_DIR/apache-activemq-$VERSION-bin.tar.gz
+sudo tar -xvzf $TMP_DIR/apache-activemq-$VERSION-bin.tar.gz -C $TMP_DIR
 sudo mv $TMP_DIR/apache-activemq-$VERSION/* $ACTIVEMQ_HOME
 
-cat << EOF >> ./activemq.service
+cat << EOF >> $TMP_DIR/activemq.service
 [Unit]
 Description=Apache ActiveMQ
 After=network.target
@@ -54,15 +54,15 @@ WantedBy=multi-user.target
 
 EOF
 
-sudo cp activemq.service /etc/systemd/system/
+sudo cp $TMP_DIR/activemq.service /etc/systemd/system/
 
 # Configure ActiveMQ access from any network
-cat $ACTIVEMQ_HOME/conf/jetty.xml | sed 's/property name=\"host\" value=\"127.0.0.1\"/property name=\"host" value=\"0.0.0.0\"/g' > $TMP_DIR/jetty-updated.xml
-sudo cp $TMP_DIR/jetty-updated.xml $ACTIVEMQ_HOME/conf/jetty.xml
+sudo cat $ACTIVEMQ_HOME/conf/jetty.xml | sed 's/property name=\"host\" value=\"127.0.0.1\"/property name=\"host" value=\"0.0.0.0\"/g' > $TMP_DIR/jetty-updated.xml
+sudo mv $TMP_DIR/jetty-updated.xml $ACTIVEMQ_HOME/conf/jetty.xml
 
 # Configure MQ Access credentials
-cat $ACTIVEMQ_HOME/conf/jetty-realm.properties | sed "s/admin: admin, admin/admin: $PASSWORD, admin/g" | sed "s/user: user, user/user: $PASSWORD, user/g" > $TMP_DIR/jetty-realm.properties
-sudo cp $TMP_DIR/jetty-realm.properties $ACTIVEMQ_HOME/conf/jetty-realm.properties
+sudo cat $ACTIVEMQ_HOME/conf/jetty-realm.properties | sed "s/admin: admin, admin/admin: $PASSWORD, admin/g" | sed "s/user: user, user/user: $PASSWORD, user/g" > $TMP_DIR/jetty-realm.properties
+sudo mv $TMP_DIR/jetty-realm.properties $ACTIVEMQ_HOME/conf/jetty-realm.properties
 
 # Configure initial queue
 wget -P $TMP_DIR https://raw.githubusercontent.com/ibm-ecosystem-lab/azure-arm-templates/$BRANCH/utilities/activemq/activemq-vm/files/activemq.xml
