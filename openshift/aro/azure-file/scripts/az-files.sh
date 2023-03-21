5b5453201143d98dbb460974c81ac701562ce0b3
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2023-03-22 09:25:57 +1100

    Adds a loop for cluster login
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- openshift/aro/azure-file/scripts/az-files.sh
+++ openshift/aro/azure-file/scripts/az-files.sh
@@ -77,13 +77,25 @@ else
     echo "INFO: Using existing Azure CLI login"
 fi
 
+exit
+
 #######
 # Login to cluster
 if ! ${BIN_DIR}/oc status 1> /dev/null 2> /dev/null; then
     echo "INFO: Logging into OpenShift cluster $ARO_CLUSTER"
     API_SERVER=$(az aro list --query "[?contains(name,'$ARO_CLUSTER')].[apiserverProfile.url]" -o tsv)
     CLUSTER_PASSWORD=$(az aro list-credentials --name $ARO_CLUSTER --resource-group $RESOURCE_GROUP --query kubeadminPassword -o tsv)
-    ${BIN_DIR}/oc login $API_SERVER -u kubeadmin -p $CLUSTER_PASSWORD
+    # Below loop added to allow authentication service to start on new clusters
+    count=0
+    while ! ${BIN_DIR}/oc login $API_SERVER -u kubeadmin -p $CLUSTER_PASSWORD > /dev/null 2>&1 ; do
+        echo "INFO: Waiting to log into cluster. Waited $count minute. Will wait up to 15 minutes."
+        sleep 60
+        count=$(( $count + 1 ))
+        if (( $count > 15 )); then
+            echo "ERROR: Timeout waiting to log into cluster"
+            exit 1;    
+        fi
+    done
 else   
     CURRENT_SERVER=$(${BIN_DIR}/oc status | grep server | awk '{printf $6}' | sed -e 's#^https://##; s#/##')
     API_SERVER=$(az aro list --query "[?contains(name,'$CLUSTER')].[apiserverProfile.url]" -o tsv)
